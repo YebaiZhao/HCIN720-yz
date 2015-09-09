54a88d1401434f1652a7b778e897962c6a2e6f40
YebaiZhao <zhaoyebai@gmail.com>
2015-09-09 18:07:01 -0400

    plot works
    add SmoothieChart.js into the lib and now is better

--- ia1/Yebai Zhao_files/YZscript.js
+++ ia1/Yebai Zhao_files/YZscript.js
@@ -5,39 +5,75 @@ var public_key = 'RM1xmbzxarFlR7b065jV';
 var API_Url= 'https://data.sparkfun.com/output/' + public_key +'.json';
 //var $ = require("jquery");/
 var Rec_data;
+
+
+//for testing
+var data1;
+var data2;
+var array1=[1,2,3,3,4,4,5,6,7,8,8,1,3.4,5,6];
+var array2=[3,4,5,6,7,8,3,4,5,6,7.7,8,9,7];
+var arrayIndex=0;
+
 //Adapted from http://phant.io/docs/output/http/
  $.ajax({
-   url: API_Url,  //Using updated time and date!
+      url: API_Url,  //Using updated time and date!
    //url: 'https://data.sparkfun.com/output/RM1xmbzxarFlR7b065jV.json',
-   jsonp: 'callback',
-   cache: true,
-   dataType: 'jsonp',
-   data: {
-     page: 1
-   },
-   success: function(response) {
+      jsonp: 'callback',
+      cache: true,
+      dataType: 'jsonp',
+      data: {
+        page: 1
+      },
+      success: function(response) {
      // response will be a javascript
      // array of objects
      //console.log(response);
 
-   }
- })
-  .done(function(data){
-    if(console&&console.log){
-      console.log(data.slice(0,10));
-      //Rec_data=$.parseJSON(data);
-      console.log(data.length);//works
-      console.info(data[1].timestamp);//works
-      for(i=0;i<data.length;i++){
-
-        var path =new Path.Circle({
-          center: [data[i].pv_power_watts,i*10],
-          radius: 5,
-          fillColor: "white"
-        });
       }
-    }
-  }
- );
+  });
+
 
 //console.log(Rec_data);
+
+
+//Timer creater
+setInterval(refreshData,10000)
+
+
+function refreshData(){
+  data1=array1[arrayIndex];
+  data2=array2[arrayIndex];
+  arrayIndex++;
+}
+
+
+//The following adapted from http://smoothiecharts.org/tutorial.html
+var smoothie = new SmoothieChart(
+    {
+      millisPerPixel:100,
+      //interpolation:'step',  //potional 
+      grid: { strokeStyle:'rgb(64, 64, 64)', fillStyle:'rgb(10, 10, 10)',
+            lineWidth: 1, millisPerLine: 750, verticalSections: 6, },
+      labels: { fillStyle:'rgb(128, 128, 128)' },
+      timestampFormatter:SmoothieChart.timeFormatter//this shows real time , not data sample time!!
+    }
+  );
+smoothie.streamTo(document.getElementById("mycanvas"));
+// Data
+var line1 = new TimeSeries();
+var line2 = new TimeSeries();
+
+// Add a random value to each line every second
+setInterval(function() {
+  line1.append(new Date().getTime(), data1);
+  line2.append(new Date().getTime(), data2);
+}, 10000);
+
+// Add to SmoothieChart
+smoothie.addTimeSeries(line1);
+//smoothie.addTimeSeries(line1,{ strokeStyle:'rgb(0, 255, 0)', fillStyle:'rgba(0, 255, 0, 0.4)', lineWidth:3 });
+smoothie.addTimeSeries(line2);
+//smoothie.addTimeSeries(line2,{ strokeStyle:'rgb(255, 0, 255)', fillStyle:'rgba(255, 0, 255, 0.3)', lineWidth:3 });
+
+var smoothie = new SmoothieChart();
+
