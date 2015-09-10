539780653d36529469bf02e9dd3ccc9726a6bc8e
YebaiZhao <zhaoyebai@gmail.com>
2015-09-10 13:33:46 -0400

    don't remember this change

--- ia1/temp.js
+++ ia1/temp.js
@@ -9,3 +9,15 @@
     }
   }
  );
+
+
+
+  var chart = new SmoothieChart(
+    {millisPerPixel:100,
+      interpolation:'linear',
+      grid:{millisPerLine:6000}}),
+    canvas = document.getElementById('smoothie-chart'),
+    series = new TimeSeries();
+
+chart.addTimeSeries(series, {lineWidth:2,strokeStyle:'#00ff00'});
+chart.streamTo(canvas, 500);
