f75eb61b1e0d1f3c4e99745341a40e451088180d
YebaiZhao <zhaoyebai@gmail.com>
2015-09-09 22:02:02 -0400

    Almost working
    Now I need to add some style and text.

--- ia1/Yebai Zhao_files/YZscript.js
+++ ia1/Yebai Zhao_files/YZscript.js
@@ -55,74 +55,72 @@ function getData(){
 }
 
 
-makeChart("mycanvas_1");
-//The following adapted from http://smoothiecharts.org/tutorial.html
-function makeChart(mycanvasID){
 
-  var smoothie = new SmoothieChart(
+//The following adapted from http://smoothiecharts.org/tutorial.html
+
+  var smoothie1 = new SmoothieChart(
     {
       millisPerPixel:1000,
-      interpolation:'linear',  //optional 
+      //interpolation:'linear',  //optional 
       grid: { strokeStyle:'rgb(64, 64, 64)', fillStyle:'rgb(10, 10, 10)',
             lineWidth: 1, millisPerLine: 50000, verticalSections: 6, },
       labels: { fillStyle:'rgb(128, 128, 128)' },
       //timestampFormatter:function(){return timestamp;}//this shows real time , not data sample time!!
     }
   );
+    // Data 
+  var line1 = new TimeSeries();
+  var line2 = new TimeSeries();
+  var line3 = new TimeSeries();
+
+ 
    setInterval(function() {
     line1.append(new Date().getTime(), (pv_voltage_volts/10));
     line2.append(new Date().getTime(), pv_current_amp);
     line3.append(new Date().getTime(), pv_power_watts);
   }, 10000);
 
-  // Data 
-  var line1 = new TimeSeries();
-  var line2 = new TimeSeries();
-  var line3 = new TimeSeries();
 
- 
   // Add to SmoothieChart
   //smoothie.addTimeSeries(line1);
-  smoothie.addTimeSeries(line1,{ strokeStyle:'#ff0000', fillStyle:'rgba(128, 0, 0, 0.4)', lineWidth:3 });
-  smoothie.addTimeSeries(line2,{ strokeStyle:'#00ff00', fillStyle:'rgba(0, 128, 0, 0.4)', lineWidth:3 });
-  smoothie.addTimeSeries(line3,{ strokeStyle:'#0000ff', fillStyle:'rgba(0, 0, 128, 0.4)', lineWidth:3 });
+  smoothie1.addTimeSeries(line1,{ strokeStyle:'#ff0000', fillStyle:'rgba(128, 0, 0, 0.4)', lineWidth:3 });
+  smoothie1.addTimeSeries(line2,{ strokeStyle:'#00ff00', fillStyle:'rgba(0, 128, 0, 0.4)', lineWidth:3 });
+  smoothie1.addTimeSeries(line3,{ strokeStyle:'#0000ff', fillStyle:'rgba(0, 0, 128, 0.4)', lineWidth:3 });
 
-  smoothie.streamTo(document.getElementById(mycanvasID));
-}
+  smoothie1.streamTo(document.getElementById("mycanvas_1"));
 
-makeChart("mycanvas_2");
-//The following adapted from http://smoothiecharts.org/tutorial.html
-function makeChart(mycanvasID){
 
-  var smoothie = new SmoothieChart(
+
+
+
+
+  var smoothie2 = new SmoothieChart(
     {
       millisPerPixel:1000,
-      interpolation:'linear',  //optional 
+      //interpolation:'linear',  //optional 
       grid: { strokeStyle:'rgb(64, 64, 64)', fillStyle:'rgb(10, 10, 10)',
             lineWidth: 1, millisPerLine: 50000, verticalSections: 6, },
       labels: { fillStyle:'rgb(128, 128, 128)' },
       //timestampFormatter:function(){return timestamp;}//this shows real time , not data sample time!!
     }
   );
+    // Data 
+  var line4 = new TimeSeries();
+  var line5 = new TimeSeries();
+  var line6 = new TimeSeries();
    setInterval(function() {
-    line1.append(new Date().getTime(), voltage_dc);
-    line2.append(new Date().getTime(), 0);
-    line3.append(new Date().getTime(), 0);
+    line4.append(new Date().getTime(), voltage_dc);
+    line5.append(new Date().getTime(), 0);
+    line6.append(new Date().getTime(), 0);
   }, 10000);
-
-  // Data 
-  var line1 = new TimeSeries();
-  var line2 = new TimeSeries();
-  var line3 = new TimeSeries();
-
  
   // Add to SmoothieChart
   //smoothie.addTimeSeries(line1);
-  smoothie.addTimeSeries(line1,{ strokeStyle:'#ff0000', fillStyle:'rgba(128, 0, 0, 0.4)', lineWidth:3 });
-  smoothie.addTimeSeries(line2,{ strokeStyle:'#00ff00', fillStyle:'rgba(0, 128, 0, 0.4)', lineWidth:3 });
-  smoothie.addTimeSeries(line3,{ strokeStyle:'#0000ff', fillStyle:'rgba(0, 0, 128, 0.4)', lineWidth:3 });
+  smoothie2.addTimeSeries(line4,{ strokeStyle:'#ff0000', fillStyle:'rgba(128, 0, 0, 0.4)', lineWidth:3 });
+  smoothie2.addTimeSeries(line5,{ strokeStyle:'#00ff00', fillStyle:'rgba(0, 128, 0, 0.4)', lineWidth:3 });
+  smoothie2.addTimeSeries(line6,{ strokeStyle:'#0000ff', fillStyle:'rgba(0, 0, 128, 0.4)', lineWidth:3 });
 
-  smoothie.streamTo(document.getElementById(mycanvasID));
-}
+  smoothie2.streamTo(document.getElementById("mycanvas_2"));
+
 
 
